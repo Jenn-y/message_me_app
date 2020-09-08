3b872ebb95a544037756e7faa9d1cce5b10d16e8
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-09-08 12:11:05 +0200

    implemented session create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,29 @@
 class SessionsController < ApplicationController
+  #before_action :user_params, only: [:new, :create]
 
   def new
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Incorrect credentials!"
+      render 'new'
+    end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out!"
+    redirect_to login_path
+  end
+
+  # def user_params
+  #   require[:user].permit[:username, :password]
+  # end
+
 end
